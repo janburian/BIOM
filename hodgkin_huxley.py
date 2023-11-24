8f5ae104ef6116357e5a6ebbfa30de01815aa219
janburian <jburian@students.zcu.cz>
2023-11-24 20:26:06 +0100

    Functional version

--- hodgkin_huxley.py
+++ hodgkin_huxley.py
@@ -17,18 +17,18 @@ E_L = -54.0  # leak reversal potential (mV)
 
 # Function representing the Hodgkin-Huxley model
 def hodgkin_huxley(y, t):
-    V_m, m, h, n = y
+    V_m, n, m, h = y
 
     # Membrane currents
-    I_Na = g_Na * m**3 * h * (V_m - E_Na)
-    I_K = g_K * n**4 * (V_m - E_K)
+    I_Na = g_Na * m ** 3 * h * (V_m - E_Na)
+    I_K = g_K * n ** 4 * (V_m - E_K)
     I_L = g_L * (V_m - E_L)
 
     # Total membrane current
-    I_ion = I_Na + I_K + I_L
+    I_total = I_Na + I_K + I_L
 
     # Differential equations
-    dV_dt = (1 / C_m) * (I_inj(t) - I_ion)
+    dV_dt = (I_inj(t) - I_total) / C_m
 
     dn_dt = alpha_n(V_m) * (1 - n) - beta_n(V_m) * n
     dm_dt = alpha_m(V_m) * (1 - m) - beta_m(V_m) * m
@@ -44,6 +44,14 @@ def I_inj(t):
 
 
 # Alpha and beta functions for m, h, and n gates
+def alpha_n(V):
+    return 0.01 * (V + 55.0) / (1.0 - np.exp(-(V + 55.0) / 10.0))
+
+
+def beta_n(V):
+    return 0.125 * np.exp(-(V + 65.0) / 80.0)
+
+
 def alpha_m(V):
     return 0.1 * (V + 40.0) / (1.0 - np.exp(-(V + 40.0) / 10.0))
 
@@ -60,25 +68,17 @@ def beta_h(V):
     return 1.0 / (1.0 + np.exp(-(V + 35.0) / 10.0))
 
 
-def alpha_n(V):
-    return 0.01 * (V + 55.0) / (1.0 - np.exp(-(V + 55.0) / 10.0))
-
-
-def beta_n(V):
-    return 0.125 * np.exp(-(V + 65.0) / 80.0)
-
-
 if __name__ == "__main__":
     # Determine the initial conditions
     resting_potential = -65.0  # mV
+    initial_n = 0.3
     initial_m = 0.05
     initial_h = 0.6
-    initial_n = 0.3
 
-    initial_conditions = [resting_potential, initial_m, initial_h, initial_n]
+    initial_conditions = [resting_potential, initial_n, initial_m, initial_h]
 
     # Time vector (Time of simulation)
-    time = np.arange(0, 50, 0.01)
+    time = np.arange(0, 50, 0.001)
 
     # Solve the differential equations using odeint
     solution = odeint(hodgkin_huxley, initial_conditions, time)
@@ -90,17 +90,16 @@ if __name__ == "__main__":
     plt.figure(figsize=(10, 6))
 
     plt.subplot(2, 1, 1)
-    plt.plot(time_inj, injection_values, label='I_inj')
+    plt.plot(time_inj, injection_values, label='Injected current')
     plt.title('External Current Injection')
-    plt.xlabel('Time (ms)')
-    plt.ylabel('Current (uA/cm^2)')
+    plt.xlabel('Time [ms]')
+    plt.ylabel('Current [uA/cm^2]')
     plt.legend()
 
-
     plt.figure(figsize=(10, 6))
-    plt.plot(time, solution[:, 0], label='V')
+    plt.plot(time, solution[:, 0], label='Voltage')
     plt.title('Hodgkin-Huxley Model')
-    plt.xlabel('Time (ms)')
-    plt.ylabel('Membrane Potential (mV)')
+    plt.xlabel('Time [ms]')
+    plt.ylabel('Membrane Potential [mV]')
     plt.legend()
     plt.show()
